6e2a91e3dc8ba4f0182694a4764c7b3c42b7d45c
merciqiao <qiaolixue@yingu.com>
2018-08-27 18:05:09 +0800

    1.表格页增加查询区和操作区

--- src/components/tablepage/tablepage.vue
+++ src/components/tablepage/tablepage.vue
@@ -1,7 +1,47 @@
 <template>
-    <div>
+    <div class="container">
+        <!-- 查询区----start -->
+        <el-form :label-position="labelPosition" :label-width="labelWidth" :inline="true" :model="formSearch" class="demo-form-inline">
+            <el-form-item label="审批人">
+                <el-input v-model="formSearch.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="活动区域">
+                <el-select v-model="formSearch.region" placeholder="活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="审批人">
+                <el-input v-model="formSearch.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="审批人">
+                <el-input v-model="formSearch.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="审批人">
+                <el-input v-model="formSearch.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="审批人">
+                <el-input v-model="formSearch.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label="审批人">
+                <el-input v-model="formSearch.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item label=" ">
+                <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+        </el-form>
+        <!-- 查询区----end -->
+        <!-- 操作区----start -->
+        <el-row class="mgb15">
+            <el-button size="small" round type="primary">新增</el-button>
+            <el-button size="small" round type="danger">批量删除</el-button>
+        </el-row>
+        <!-- 操作区----end -->
         <!-- 表格---start -->
         <el-table :data="tableData" border stripe style="width: 100%">
+            <el-table-column type="selection" width="55">
+            </el-table-column>
+
             <el-table-column prop="date" label="日期" width="180" sortable>
             </el-table-column>
             <el-table-column prop="name" label="姓名" width="180" sortable>
@@ -18,6 +58,7 @@
         <el-pagination background layout="total,sizes,prev, pager, next,jumper" :current-page="pageInfo.pageIndex" :page-size="pageInfo.pageSize" :total="pageInfo.pageTotal" :page-sizes="[5, 10, 20, 50]" @size-change="handleSizeChange" @current-change="handleCurrentChange">
         </el-pagination>
         <!-- 表格---end -->
+
         <!-- 编辑弹框---start -->
         <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
             <el-form :model="form">
@@ -32,9 +73,6 @@
                 </el-form-item>
             </el-form>
 
-
-
-
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogFormVisible = false">取 消</el-button>
                 <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
@@ -44,6 +82,14 @@
         <!-- 编辑弹框---end -->
     </div>
 </template>
+
+<style lang="scss">
+// 设置输入框的宽度
+.el-form-item__content {
+    width: 220px;
+}
+</style>
+
 <script>
 export default {
     name: 'tablepage',
@@ -76,6 +122,12 @@ export default {
                     address: "上海市普陀区金沙江路 1516 弄"
                 }
             ],
+            formSearch: {   //表单对象
+                user: '',
+                region: ''
+            },
+            labelPosition: 'right', //lable对齐方式
+            labelWidth: '80px',  //lable宽度
             form: {
                 name: '',
                 region: '',
@@ -99,7 +151,7 @@ export default {
                 message: msg,
                 type: "success"
             });
-            this.dialogFormVisible=true;
+            this.dialogFormVisible = true;
         },
         handleDelete(index, rowData) {
             var msg = "索引是:" + index + ",行内容是:" + JSON.stringify(rowData);
@@ -134,6 +186,9 @@ export default {
                 type: "success"
             });
 
+        },
+        onSubmit() {
+            console.log('submit!');
         }
     }
 };
