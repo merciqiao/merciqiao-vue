eb7a428d887ae95aa9e1e9ec918fbaa1b7725670
merciqiao <qiaolixue@yingu.com>
2018-08-31 16:17:24 +0800

    1.注释无效的菜单

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -62,49 +62,49 @@
                             }
                         ]
                     },
-                    {
-                        icon: 'el-icon-message',
-                        index: 'tabs',
-                        title: 'tab选项卡'
-                    },
-                    {
-                        icon: 'el-icon-date',
-                        index: '3',
-                        title: '表单相关',
-                        subs: [
-                            {
-                                index: 'form',
-                                title: '基本表单'
-                            },
-                            {
-                                index: 'editor',
-                                title: '富文本编辑器'
-                            },
-                            {
-                                index: 'markdown',
-                                title: 'markdown编辑器'
-                            },
-                            {
-                                index: 'upload',
-                                title: '文件上传'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-star-on',
-                        index: 'charts',
-                        title: 'schart图表'
-                    },
-                    {
-                        icon: 'el-icon-rank',
-                        index: 'drag',
-                        title: '拖拽列表'
-                    },
-                    {
-                        icon: 'el-icon-warning',
-                        index: 'permission',
-                        title: '权限测试'
-                    },
+                    // {
+                    //     icon: 'el-icon-message',
+                    //     index: 'tabs',
+                    //     title: 'tab选项卡'
+                    // },
+                    // {
+                    //     icon: 'el-icon-date',
+                    //     index: '3',
+                    //     title: '表单相关',
+                    //     subs: [
+                    //         {
+                    //             index: 'form',
+                    //             title: '基本表单'
+                    //         },
+                    //         {
+                    //             index: 'editor',
+                    //             title: '富文本编辑器'
+                    //         },
+                    //         {
+                    //             index: 'markdown',
+                    //             title: 'markdown编辑器'
+                    //         },
+                    //         {
+                    //             index: 'upload',
+                    //             title: '文件上传'
+                    //         }
+                    //     ]
+                    // },
+                    // {
+                    //     icon: 'el-icon-star-on',
+                    //     index: 'charts',
+                    //     title: 'schart图表'
+                    // },
+                    // {
+                    //     icon: 'el-icon-rank',
+                    //     index: 'drag',
+                    //     title: '拖拽列表'
+                    // },
+                    // {
+                    //     icon: 'el-icon-warning',
+                    //     index: 'permission',
+                    //     title: '权限测试'
+                    // },
                     {
                         icon: 'el-icon-error',
                         index: '404',
