c4c2f448aa03c6edbeedc1f45b019e413375260a
merciqiao <qiaolixue@yingu.com>
2018-11-01 23:49:38 +0800

    1.登陆记录ip

--- src/apis/shiroApi.js
+++ src/apis/shiroApi.js
@@ -7,18 +7,28 @@ export default {
     /**
      * 系统登陆
      */
-    loginIn:function({loginName, password}) {
-        var param={
-            loginName:loginName,
-            password:password
+    loginIn: function ({ loginName, password }) {
+        var param = {
+            loginName: loginName,
+            password: password
         };
-        return Axios.post('/api/shiro-api/login',param);
-        
+        return Axios.post('/api/shiro-api/login', param);
+
     },
     /**
      * 退出登陆
      */
-    loginOut:function(){
-         return Axios.post('/api/shiro-api/loginout');
+    loginOut: function () {
+        return Axios.post('/api/shiro-api/loginout');
+    },
+    /**
+     * 记录登陆日志
+     */ 
+    loginLog: function ({ip,city}) {
+        var param={
+            ip:ip,
+            city:city                
+        };
+        return Axios.post('/api/loginlog-api/save', param);
     }
 }
