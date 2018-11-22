963c376559f00eb5180241eb53e284a7732c4cfd
merciqiao <812858143@qq.com>
2018-11-22 22:44:27 +0800

    添加动态下拉框

--- src/components/searchinput/searchinput.vue
+++ src/components/searchinput/searchinput.vue
@@ -26,6 +26,16 @@
             <el-form-item label="审批人">
                 <el-input v-model="formInline.user" placeholder="审批人"></el-input>
             </el-form-item>
+            <el-form-item label="动态下拉框">
+                <el-select v-model="formInline.selected" placeholder="请选择">
+                    <el-option
+                        v-for="item in selectsData"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                    </el-option>
+                </el-select>
+            </el-form-item>
             <el-form-item label=" ">
                 <el-button type="primary" @click="onSubmit">查询</el-button>
             </el-form-item>
@@ -36,26 +46,43 @@
 <style lang="scss">
 // 设置输入框的宽度
 .el-form-item__content {
-     width: 220px;
+  width: 220px;
 }
 </style>
 <script>
 export default {
-    name: 'searchinput',
-    data() {
-        return {
-            formInline: {   //表单对象
-                user: '',
-                region: ''
-            },
-            labelPosition: 'right', //lable对齐方式
-            labelWidth: '80px'   //lable宽度
+  name: "searchinput",
+  data() {
+    return {
+      formInline: {
+        //表单对象
+        user: "",
+        region: "",
+        selected:''
+      },
+      labelPosition: "right", //lable对齐方式
+      labelWidth: "100px", //lable宽度
+      selectsData: [ //下拉框数据
+        {
+          value: "HTML",
+          label: "HTML"
+        },
+        {
+          value: "CSS",
+          label: "CSS"
+        },
+        {
+          value: "JavaScript",
+          label: "JavaScript"
         }
-    },
-    methods: {
-        onSubmit() {
-            console.log('submit!');
-        }
+      ]
+      
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
     }
-}
+  }
+};
 </script>
