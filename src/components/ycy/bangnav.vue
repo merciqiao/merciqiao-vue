0c566a6f32a9425ad046882c21d8826df6096931
merciqiao <qiaolixue@yingu.com>
2019-04-04 17:41:12 +0800

    1.优化榜单列表文案

--- src/components/ycy/bangnav.vue
+++ src/components/ycy/bangnav.vue
@@ -155,7 +155,7 @@ export default {
                 {
                     index: 3,
                     src: 'static/img/bang/bang3.jpg',
-                    tip: '此功能正在开发中...'
+                    tip: '此功能正在开发中,敬请期待...'
                 },
                 {
                     index: 4,
