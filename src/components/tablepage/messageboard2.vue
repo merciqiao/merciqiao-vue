140549c11e93850e9f36918947e5db8f29505f5b
merciqiao <812858143@qq.com>
2019-06-02 23:14:09 +0800

    1.使用文档增加不可删除

--- src/components/tablepage/messageboard2.vue
+++ src/components/tablepage/messageboard2.vue
@@ -397,6 +397,10 @@ export default {
          * 删除
          */
         handleDelete(index, rowData) {
+            if(rowData.name=='使用文档'){
+                this.$message('使用文档不可删除');
+                return;
+            }
             var id=rowData.id;
             this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
                 confirmButtonText: '确定',
