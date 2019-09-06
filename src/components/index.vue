4813f7564237b841fbf299694d0e1847ff50b46d
merciqiao <qiaolixue@yingu.com>
2019-09-06 13:49:20 +0800

    1.更新赞助信息

--- src/components/index.vue
+++ src/components/index.vue
@@ -10,6 +10,7 @@
     <br />版本说明:
     <br />------------------
     <br />功能版:101ycy.com:6001,此版本菜单和权限在"系统管理"页可配置
+             (或https://101ycy.com:6011)
     <br />精简版:101ycy.com:6002,此版本菜单和权限在前端代码里配置
     <br />Mock版:101ycy.com:6005,此版本无权限,菜单在前端代码写死
     <br />
@@ -23,26 +24,12 @@
     <br />------------------
     <br />详情可在页底加群问我
     <br />★ 赞助★ 请点击右上角admin处
-    <br />
-    <br />
-      非常感谢
-    <br />
-    ------------------
-    <br />
-    <div class="zanzhulist">
-      <table class="zanzhutb">
-        <tr><td>命：</td><td>赞助10元</td><td>2019-9-2</td></tr>
-        <tr><td>顺势而为：</td><td>赞助100元</td><td>2019-9-1</td></tr>
-        <tr><td>顺势而为：</td><td>赞助2元</td><td>2019-8-25</td></tr>
-        <tr><td>*商：</td><td>赞助1元</td><td>2019-8-19</td></tr>
-        <tr><td>苏州-前端-离愁：</td><td>赞助50元</td><td>2019-8-13</td></tr>
-      </table>
-    </div>
+    
 
     
     <br />
     <br />
-作者打卡:2019-8-25
+作者打卡:2019-09-06
     <br />
 
     <br />
@@ -57,6 +44,30 @@ PS:烦请修改了数据的用户,记得恢复下数据
       <img class='hongbao' src="static/img/hongbao.png">
     </div>-->
     <!-- 扫码 end -->
+
+    <br />
+    <br />
+      非常感谢
+    <br />
+    ------------------
+    <br />
+    <div class="zanzhulist">
+      <table class="zanzhutb">
+        <tr><td>懒是原罪：</td><td>赞助15元</td><td>2019-9-6</td></tr>
+        <tr><td>河北：</td><td>赞助0.8元</td><td>2019-9-6</td></tr>
+        <tr><td>武汉-后端-星空：</td><td>赞助1元</td><td>2019-9-6</td></tr>
+        <tr><td>珠海-全栈-全干：</td><td>赞助5元</td><td>2019-9-6</td></tr>
+        <tr><td>深圳-杜雪：</td><td>赞助5元</td><td>2019-9-6</td></tr>
+        <tr><td>深圳-前端-亲亲：</td><td>赞助2.22元</td><td>2019-9-6</td></tr>
+        <tr><td>北京-我乖你给我买街：</td><td>赞助0.55元</td><td>2019-9-6</td></tr>
+        <tr><td>卖提莫的小男孩：</td><td>赞助5元</td><td>2019-9-6</td></tr>
+        <tr><td>命：</td><td>赞助10元</td><td>2019-9-2</td></tr>
+        <tr><td>顺势而为：</td><td>赞助100元</td><td>2019-9-1</td></tr>
+        <tr><td>顺势而为：</td><td>赞助2元</td><td>2019-8-25</td></tr>
+        <tr><td>*商：</td><td>赞助1元</td><td>2019-8-19</td></tr>
+        <tr><td>苏州-前端-离愁：</td><td>赞助50元</td><td>2019-8-13</td></tr>
+      </table>
+    </div>
   </div>
 </template>
 <style>
